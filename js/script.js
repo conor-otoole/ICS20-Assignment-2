1a4fd77a226f8181deac71cdeb79215d10cb10de
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2022-10-25 01:49:41 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* Created by: Conor
+* Created on: Oct 2022
+* This file contains the JavaScript for index.html Assignment#1
+*/
+/* Function */
+function calculate() {
+  /* Input */
+  const base = parseInt(document.getElementById("parallelogramBase").value)
+    const base = parseInt(document.getElementById("parallelogramHeight").value)
+
+  /* Process */
+  const area = base * height
+
+  /* Output */
+  document.getElementById(*area*). innerHTML = "The area is: " + area + " cm²"
+}
