46bfe5d4a17b666da04b20e5cc192e698f3481a1
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2022-10-26 00:34:36 +0000

    ieguohjdrto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 /* Created by: Conor
 * Created on: Oct 2022
-* This file contains the JavaScript for index.html Assignment#1
+* This file contains the JavaScript for index.html Assignment#2
 */
-/* Function */
-function calculate() {
-  /* Input */
-  const base = parseInt(document.getElementById("parallelogramBase").value)
-    const base = parseInt(document.getElementById("parallelogramHeight").value)
 
-  /* Process */
+'use strict'
+/**
+/* Function */
+   function calculate() {
+  // Input 
+  const base = parseInt(document.getElementById("length-of-parallelogram").value)
+    const height = parseInt(document.getElementById("width-of-parallelogram").value)
+
+  // Process 
   const area = base * height
 
-  /* Output */
-  document.getElementById(*area*). innerHTML = "The area is: " + area + " cm²"
+  // Output 
+  document.getElementById("area").innerHTML = "The area is:" + area + "cm²"
 }
